1d8ae4e769102c9831bfdb366f44b23705dfa898
drmcbride12 <130199368+drmcbride12@users.noreply.github.com>
2023-06-04 03:29:16 -0400

    Update screen_capture.py

--- screen_capture.py
+++ screen_capture.py
@@ -1,40 +1,67 @@
 import pyautogui
+from pynput import mouse, keyboard
+import time
+import os
 import cv2
 import numpy as np
 
-# Specify resolution
-resolution = (1920, 1080)
+# Create a directory to save screenshots
+os.makedirs('screenshots', exist_ok=True)
 
-# Specify video codec
-codec = cv2.VideoWriter_fourcc(*"XVID")
+# Output file for actions
+output_file = open('output.txt', 'w')
 
-# Create video write object
-out = cv2.VideoWriter("recorded.avi", codec, 30.0, resolution)
+# Colors
+colors = [
+    np.array([255, 0, 0]),  # Red
+    np.array([0, 255, 0]),  # Green
+    np.array([0, 0, 255]),  # Blue
+    np.array([0, 0, 0]),    # Black
+    np.array([255, 255, 255]) # White
+]
 
-cv2.namedWindow("Recording", cv2.WINDOW_NORMAL)
+def reduce_colors(img):
+    # Euclidean distance between the image and each of the colors
+    distances = [np.linalg.norm(img - color, axis=-1) for color in colors]
 
-# Resize this window
-cv2.resizeWindow("Recording", 480, 270)
+    # Create a new image, starting off all black (color 3)
+    new_img = np.zeros(img.shape, np.uint8)
+    for i in range(4):
+        new_img[distances[i] < distances[i + 1]] = colors[i]
 
-while True:
-    # Capture screenshot
-    img = pyautogui.screenshot()
-    
-    # Convert the image into numpy array representation
-    frame = np.array(img)
-    
-    # Convert the BGR image into RGB image
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # Write RBG image to file
-    out.write(frame)
-    
-    # Display screen/frame being recorded
-    cv2.imshow('Recording', frame)
-    
-    # Wait for the user to press 'q' key to stop the recording
-    if cv2.waitKey(1) == ord('q'):
-        break
+    return new_img
 
-out.release()  # close the video file
-cv2.destroyAllWindows()  # close the all openCV windows
+# Mouse and keyboard listeners
+def on_click(x, y, button, pressed):
+    output_file.write(f'Mouse {button} {"Pressed" if pressed else "Released"} at {(x, y)}, {time.time()}\n')
+    screenshot = pyautogui.screenshot()
+    screenshot = np.array(screenshot)
+    screenshot = cv2.resize(screenshot, (320, 180))
+    screenshot = reduce_colors(screenshot)
+    cv2.imwrite(f'screenshots/{time.time()}_click.png', screenshot)
+
+def on_key_press(key):
+    x, y = pyautogui.position()
+    output_file.write(f'Key {key} pressed at {(x, y)}, {time.time()}\n')
+    screenshot = pyautogui.screenshot()
+    screenshot = np.array(screenshot)
+    screenshot = cv2.resize(screenshot, (320, 180))
+    screenshot = reduce_colors(screenshot)
+    cv2.imwrite(f'screenshots/{time.time()}_keypress.png', screenshot)
+
+mouse_listener = mouse.Listener(on_click=on_click)
+keyboard_listener = keyboard.Listener(on_press=on_key_press)
+
+# Start listeners
+mouse_listener.start()
+keyboard_listener.start()
+
+try:
+    # Keep the script running.
+    while True:
+        pass
+except KeyboardInterrupt:
+    # When Ctrl+C is pressed, stop listeners and close the output file.
+    mouse_listener.stop()
+    keyboard_listener.stop()
+    output_file.close()
